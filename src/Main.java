bc1acfa021a0ec2de33476d5002f3f5aca91e2ad
Juan Arguello <100389536+Arguellis17@users.noreply.github.com>
2025-03-19 14:23:18 -0500

    He modificado el mensaje de prueba

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        System.out.println("Mensaje de prueba xd");
+        System.out.println("Modificando mensaje");
     }
 }
