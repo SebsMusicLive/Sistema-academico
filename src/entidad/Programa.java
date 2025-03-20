35b57060bacca0eedfbbf964287d93a609db167d
Juan Arguello <100389536+Arguellis17@users.noreply.github.com>
2025-03-19 23:48:56 -0500

    Se han agregado los metodos y un atributo más a la clase de Programa

--- src/entidad/Programa.java
+++ src/entidad/Programa.java
@@ -6,132 +6,62 @@ import java.util.*;
  * 
  */
 public class Programa {
-
-    /**
-     * Default constructor
-     */
-    public Programa() {
-    }
-
-    /**
-     * 
-     */
     private String codigo_programa;
-
-    /**
-     * 
-     */
     private String nombre;
-
-    /**
-     * 
-     */
     private String descripcion;
-
-    /**
-     * 
-     */
     private Carrera codigo_carrera;
+    private List<Curso> cursos;
 
-    /**
-     * @return
-     */
+    public Programa() {
+        this.cursos = new ArrayList<>();
+    }
+
     public String getCodigo_programa() {
-        // TODO implement here
-        return "";
+        return codigo_programa;
     }
 
-    /**
-     * @param codigo_programa 
-     * @return
-     */
     public void setCodigo_programa(String codigo_programa) {
-        // TODO implement here
-        return null;
+        this.codigo_programa = codigo_programa;
     }
 
-    /**
-     * @return
-     */
     public String getNombre() {
-        // TODO implement here
-        return "";
+        return nombre;
     }
 
-    /**
-     * @param nombre 
-     * @return
-     */
     public void setNombre(String nombre) {
-        // TODO implement here
-        return null;
+        this.nombre = nombre;
     }
 
-    /**
-     * @return
-     */
     public String getDescripcion() {
-        // TODO implement here
-        return "";
+        return descripcion;
     }
 
-    /**
-     * @param descripcion 
-     * @return
-     */
     public void setDescripcion(String descripcion) {
-        // TODO implement here
-        return null;
+        this.descripcion = descripcion;
     }
 
-    /**
-     * @return
-     */
     public Carrera getCodigoCarrera() {
-        // TODO implement here
-        return null;
+        return codigo_carrera;
     }
 
-    /**
-     * @param codigo_carrera 
-     * @return
-     */
     public void setCodigoCarrera(Carrera codigo_carrera) {
-        // TODO implement here
-        return null;
+        this.codigo_carrera = codigo_carrera;
     }
 
-    /**
-     * @return
-     */
     public void registrarPrograma() {
-        // TODO implement here
-        return null;
+        System.out.println("Programa " + nombre + " registrado exitosamente.");
     }
 
-    /**
-     * @return
-     */
     public void eliminarPrograma() {
-        // TODO implement here
-        return null;
+        System.out.println("Programa " + nombre + " eliminado exitosamente.");
     }
 
-    /**
-     * @param curso 
-     * @return
-     */
     public void agregarCurso(Curso curso) {
-        // TODO implement here
-        return null;
+        cursos.add(curso);
+        System.out.println("Curso agregado al programa " + nombre);
     }
 
-    /**
-     * @return
-     */
     public void actualizarContenidos() {
-        // TODO implement here
-        return null;
+        System.out.println("Contenidos del programa " + nombre + " actualizados.");
     }
-
 }
